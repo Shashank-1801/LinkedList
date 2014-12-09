2e9e60398d9d6996f4d53a163c1517329a4520fc
Shashank Shekhar <shashankeshav@gmail.com>
2014-12-09 22:25:10 +0530

    Added 2 more impl for isCylcic 1. Hash Table 2. Flyod Algo

--- src/problemsOfLinkedLists/CyclicLinkedListProblem.java
+++ src/problemsOfLinkedLists/CyclicLinkedListProblem.java
@@ -1,6 +1,8 @@
 package problemsOfLinkedLists;
 
+import java.util.Hashtable;
 import java.util.Random;
+
 import singleLinkedList.Node;
 import singleLinkedList.SingleLinkedList;
 
@@ -25,9 +27,21 @@ public class CyclicLinkedListProblem {
 		}
 		System.out.println("Last node at " + last.getData());
 		last.setNext(nodeRandomfromList(L2));
-		
+
+		System.out.println("#########################");
+
 		System.out.println(isCyclic(L1));
 		System.out.println(isCyclic(L2));
+		
+		System.out.println("#########################");
+
+		System.out.println(isCyclicHashTable(L1));
+		System.out.println(isCyclicHashTable(L2));
+		
+		System.out.println("#########################");
+
+		System.out.println(isCyclicFloyd(L1));
+		System.out.println(isCyclicFloyd(L2));
 		
 		System.out.println("#########################");
 		
@@ -40,14 +54,54 @@ public class CyclicLinkedListProblem {
 			temp=temp.getNext();
 			Node temp2 = List.getHead();
 			for(int i=0; i<count ; i++){
-				if(temp == temp2)
+				if(temp == temp2){
+					System.out.println("Cyclic at "+ temp.getData());
 					return true;
+				}
 				else{
 					temp2 = temp2.getNext();
 				}
 			}
 			count++;
 			
+		}
+		
+		return false;
+	}
+	
+	public static boolean isCyclicFloyd(SingleLinkedList List){
+		Node fastptr = List.getHead();
+		Node slowptr = List.getHead();
+		int count = 1;
+		while(fastptr!=null ){
+			if(count%2 == 0){
+				slowptr = slowptr.getNext();
+			}
+			fastptr = fastptr.getNext();
+			if(fastptr == slowptr){
+				System.out.println("Cyclic maybe at "+ fastptr.getData());
+				return true;
+			}
+			count ++;			
+		}
+		
+		return false;
+	}
+	
+	public static boolean isCyclicHashTable(SingleLinkedList List){
+		Node temp = List.getHead();
+		Hashtable<Integer, Node> hashLinkedList = new Hashtable<Integer, Node>();
+		while(temp!=null ){
+			hashLinkedList.put(temp.getData(), temp);
+			//System.out.println("key = "+ temp.getData() + " and Value = "+ temp + " or " + hashLinkedList.get(temp.getData()) );
+			temp=temp.getNext();
+			if(temp!=null){
+				if(temp == hashLinkedList.get((temp.getData()))){
+					System.out.println("Cyclic at "+ temp.getData());
+					return true;
+				}
+			}
+									
 		}
 		
 		return false;
